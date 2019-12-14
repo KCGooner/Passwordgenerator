eb873676f81f48ebde198c07984a9bb797a9f449
Joe Huelsman <kcgooner91@gmail.com>
2019-12-13 20:51:54 -0600

    moved files

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var specialCharacters = ["!", "#", "@", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "?", "/", "<", ">"]
+var numbers = ["1","2","3","4","5","6","7","8","9","0"]
+var characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+
+
 
 // Write password to the #password input
 function writePassword() {
