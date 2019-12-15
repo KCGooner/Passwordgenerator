cb22e7d49294af706829f49ee5709eed047b7ed2
Joe Huelsman <kcgooner91@gmail.com>
2019-12-14 20:53:46 -0600

    added confirm statements

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ var specialCharacters = ["!", "#", "@", "$", "%", "^", "&", "*", "(", ")", "-", 
 var numbers = ["1","2","3","4","5","6","7","8","9","0"]
 var characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
+var confirmSpecial = confirm("Do you want special characters?");
+var confirmNumber = confirm("Do you want numbers?");
+var confirmCharacters = confirm("Do you want characters?");
 
+if (confirmSpecial) {
+  
+}
 
 // Write password to the #password input
 function writePassword() {
