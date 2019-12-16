04d1385f8930aa815926a59851c5ec1673945c1a
Joe Huelsman <kcgooner91@gmail.com>
2019-12-16 13:05:34 -0600

    javascript update

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ var generateBtn = document.querySelector("#generate");
 
 var specialCharacters = ["!", "#", "@", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "?", "/", "<", ">"]
 var numbers = ["1","2","3","4","5","6","7","8","9","0"]
-var characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var upperCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var lowerCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-// var confirmSpecial = confirm("Do you want special characters?");
-// var confirmNumber = confirm("Do you want numbers?");
-// var confirmCharacters = confirm("Do you want characters?");
 
-if (confirmSpecial) {
-  
-}
+specialCharacters = confirm("Do you want special characters?");
+numbers = confirm("Do you want numbers?");
+upperCharacters = confirm("Do you want uppercase characters?");
+lowerCharacters = confirm('Do you want lowercase characters?');
+
+
 
 // Write password to the #password input
 function writePassword() {
