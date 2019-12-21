bf58e1e4132da00b564c2f6a5c12d9f129dc1b2a
Joe Huelsman <kcgooner91@gmail.com>
2019-12-21 10:10:52 -0600

    fixed substring errors

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// var numbers = ['1','2','3','4','5','6','7','8','9','0'];
-// var specialCharacters = ['!','@','#','$','%','^','&','*','(',')'];
-// var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-// var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
 var passwordLength = prompt("How many characters do you want password to be? ");
 
 if(passwordLength <= 7 || passwordLength >= 129) {
@@ -21,13 +16,13 @@ if (uCase) {
   avail += values.substring(0,26);
 }
 if (lCase) {
-  avail += values.substring(26,26);
+  avail += values.substring(26,52);
 }
 if (numberz) {
-  avail += values.substring(52,10);
+  avail += values.substring(52,62);
 }
 if (sCharacters) {
-  avail += values.substring(62,10);
+  avail += values.substring(62);
 }
 
 
